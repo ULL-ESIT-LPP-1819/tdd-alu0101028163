745af9792c6d9ed0b982b45669dc672541737d48
alu0101028163 <alu0101028163@ull.edu.es>
2018-11-13 16:09:05 +0000

    Incrementado el tamaño al hacer push_back

--- practica6/lib/list.rb
+++ practica6/lib/list.rb
@@ -12,12 +12,14 @@ def push_back(node)
   aux = @tail
   @tail = node
   node.prev = aux
+  @size += 1
 end
 
 def push_front(node)
   aux = @head
   @head = node
   node.next = aux
+  @size += 1
 end
 
 
--- practica6/spec/list_spec.rb
+++ practica6/spec/list_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe List do
 
 before :each do
   @lista = List.new()
-  @nodo = Struct.new(:prev, :next, :value).new(nil,nil,Etiqueta.new([2380.24, 572.76], [17.3,10,12], [41.2,4.15,5.12], 3.3 , 2.4, 1.2, 0, false, 0, 0))
+  @nodo1 = Struct.new(:prev, :next, :value).new(nil,nil,Etiqueta.new([2380.24, 572.76], [17.3,10,12], [41.2,4.15,5.12], 3.3 , 2.4, 1.2, 0, false, 0, 0))
+  @nodo2 = Struct.new(:prev, :next, :value).new(nil,nil,Etiqueta.new([2380.24, 572.76], [17.3,10,12], [41.2,4.15,5.12], 3.3 , 2.4, 1.2, 0, false, 0, 0))
+
 end
 
   context "Habiendo creado una lista" do
+
       it "Los valores de head y tail deberían ser nil" do
         expect(@lista.head).to eq(nil)
         expect(@lista.tail).to eq(nil)
@@ -19,17 +22,17 @@ end
       context "Cuando empujamos un elemento por medio de un push_back" do
               it "El nodo debería apuntar al nodo al que previamente apuntaba tail" do
                 nodo_previo_tail = @lista.tail
-                @lista.push_back(@nodo)
-                expect(@nodo.prev).to eq(nodo_previo_tail)
+                @lista.push_back(@nodo1)
+                expect(@nodo1.prev).to eq(nodo_previo_tail)
               end
               it "Tail debería de apuntar al nuevo nodo" do
-                @lista.push_back(@nodo)
-                expect(@lista.tail).to eq(@nodo)
+                @lista.push_back(@nodo1)
+                expect(@lista.tail).to eq(@nodo1)
               end
 
               it "Y el tamaño debería incrementarse en una unidad " do
-                @lista.push_back(@nodo)
                 actual_size = @lista.size
+                @lista.push_back(@nodo1)
                 expect(@lista.size).to eq(actual_size + 1)
               end
         end
@@ -37,20 +40,31 @@ end
         context "Cuando empujamos un elemento por medio de un push_front" do
                 it "El nodo debería apuntar al nodo al que previamente apuntaba head" do
                   nodo_previo_head = @lista.head
-                  @lista.push_front(@nodo)
-                  expect(@nodo.next).to eq(nodo_previo_head)
+                  @lista.push_front(@nodo1)
+                  expect(@nodo1.next).to eq(nodo_previo_head)
                 end
                 it "y Head debería de apuntar al nuevo nodo" do
-                  @lista.push_front(@nodo)
-                  expect(@lista.head).to eq(@nodo)
+                  @lista.push_front(@nodo1)
+                  expect(@lista.head).to eq(@nodo1)
                 end
 
                 it "Y el tamaño debería incrementarse en una unidad " do
-                  @lista.push_front(@nodo)
                   actual_size = @lista.size
+                  @lista.push_front(@nodo1)
                   expect(@lista.size).to eq(actual_size + 1)
                 end
           end
+
+          context "Cuando expulsamos un elemento por medio de pop_front" do
+              it "Head debería de apuntar al nodo siguiente del que apuntada" do
+                @lista.push_front(@nodo1)
+                @lista.push_front(@nodo2)
+                expect(@lista.head).to eq(@nodo2)
+                @lista.pop_front
+                expect(@lista.head).to eq(@nodo1)
+              end
+          end
+
     end
 
   end
