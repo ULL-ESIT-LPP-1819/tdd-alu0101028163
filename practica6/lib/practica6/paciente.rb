0a54b80a3d1c91717f4a5b511b75d99329ee8f6c
= <alu0101028163@ull.edu.es>
2018-11-22 16:01:56 +0000

    Error, no existe la clase Paciente_Obeso

--- practica6/lib/practica6/paciente.rb
+++ practica6/lib/practica6/paciente.rb
@@ -1,7 +1,7 @@
 class Paciente < Persona
 
 attr_reader :cir_cintura, :cir_cadera, :cir_brazo, :pl_tricipital, :pl_bicipital, :pl_subescapular, :pl_suprailiaco
-  def initialize(nombre, peso, talla, edad, sexo, cir_cintura, cir_cadera, cir_brazo, pl_tricipital, pl_bicipital, pl_subescapular, pl_suprailiaco)
+  def initialize(nombre, peso, talla, edad, sexo, cir_cintura = nil, cir_cadera = nil, cir_brazo = nil, pl_tricipital = nil, pl_bicipital = nil, pl_subescapular = nil, pl_suprailiaco = nil)
     super(nombre, peso, talla, edad, sexo)
     @cir_cintura = cir_cintura
     @cir_cadera = cir_cadera
@@ -11,4 +11,17 @@ attr_reader :cir_cintura, :cir_cadera, :cir_brazo, :pl_tricipital, :pl_bicipital
     @pl_subescapular = pl_subescapular
     @pl_suprailiaco = pl_suprailiaco
   end
+
+  def tiene_obesidad
+    x = false
+    if !cir_cintura.nil? and !cir_cadera.nil? and !cir_brazo.nil? and
+       !pl_tricipital.nil? and !pl_bicipital.nil? and !pl_subescapular.nil? and
+       !pl_suprailiaco.nil?
+         x = true
+    else
+        x = false
+    end
+    x
+  end
+
 end
---
+++ practica6/spec/paciente_obeso_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper.rb"
+
+
+RSpec.describe Paciente_obeso do
+  
+end
