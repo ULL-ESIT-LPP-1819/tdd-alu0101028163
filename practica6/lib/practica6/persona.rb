4311ea2c629c09b52063a37f40032f8c3f10ff4a
= <alu0101028163@ull.edu.es>
2018-11-22 12:57:23 +0000

    Añadidos los atributos correspondientes a las circunferencias

--- practica6/lib/practica6/persona.rb
+++ practica6/lib/practica6/persona.rb
@@ -1,12 +1,15 @@
 class Persona
 
-  attr_reader :nombre, :peso, :talla, :edad, :sexo
-  def initialize(nombre, peso, talla, edad, sexo)
+  attr_reader :nombre, :peso, :talla, :edad, :sexo, :cir_cintura, :cir_cadera, :cir_brazo
+  def initialize(nombre, peso, talla, edad, sexo, cir_cintura, cir_cadera, cir_brazo)
     @nombre = nombre
     @peso = peso
     @talla = talla
     @edad = edad
     @sexo = sexo
+    @cir_cintura = cir_cintura
+    @cir_cadera = cir_cadera
+    @cir_brazo = cir_brazo
   end
 
 end
