c3856e258858a7ee211571ee90b795f2aaac8ed8
= <alu0101028163@ull.edu.es>
2018-11-22 13:02:46 +0000

    Añadidos los pliegues

--- practica6/lib/practica6/persona.rb
+++ practica6/lib/practica6/persona.rb
@@ -1,7 +1,7 @@
 class Persona
 
-  attr_reader :nombre, :peso, :talla, :edad, :sexo, :cir_cintura, :cir_cadera, :cir_brazo
-  def initialize(nombre, peso, talla, edad, sexo, cir_cintura, cir_cadera, cir_brazo)
+  attr_reader :nombre, :peso, :talla, :edad, :sexo, :cir_cintura, :cir_cadera, :cir_brazo, :pl_tricipital, :pl_bicipital, :pl_subescapular, :pl_suprailiaco
+  def initialize(nombre, peso, talla, edad, sexo, cir_cintura, cir_cadera, cir_brazo, pl_tricipital, pl_bicipital, pl_subescapular, pl_suprailiaco)
     @nombre = nombre
     @peso = peso
     @talla = talla
@@ -10,6 +10,10 @@ class Persona
     @cir_cintura = cir_cintura
     @cir_cadera = cir_cadera
     @cir_brazo = cir_brazo
+    @pl_tricipital = pl_tricipital
+    @pl_bicipital = pl_bicipital
+    @pl_subescapular = pl_subescapular
+    @pl_suprailiaco = pl_suprailiaco
   end
 
 end
--- practica6/spec/persona_spec.rb
+++ practica6/spec/persona_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe Persona do
 
   before :all do
-    @persona = Persona.new("Adrian", 81.5, 1.68, 20, 0, 135.23, 146.32, 32.2)
+    @persona = Persona.new("Adrian", 81.5, 1.68, 20, 0, 135.23, 146.32, 32.2, 23.4, 24.4, 26.4, 28.4)
   end
 
   context "Habiendo una persona" do
@@ -23,7 +23,7 @@ RSpec.describe Persona do
       expect(@persona.sexo).to eq(0)
     end
 
-    context "Debe tener definidas las circufenrencias" do
+    context "Debe tener definidas las circunferencias" do
       it "de su cintura" do
         expect(@persona.cir_cintura).to eq(135.23)
       end
@@ -33,6 +33,22 @@ RSpec.describe Persona do
       it "de su brazo" do
         expect(@persona.cir_brazo).to eq(32.2)
       end
+    end
+
+    context "Debe tener definidos los pliegues" do
+      it "tricipital" do
+        expect(@persona.pl_tricipital).to eq(23.4)
+      end
+      it "bicipital" do
+        expect(@persona.pl_bicipital).to eq(24.4)
+      end
+      it "subescapular" do
+        expect(@persona.pl_subescapular).to eq(26.4)
+      end
+      it "suprailiaco" do
+        expect(@persona.pl_suprailiaco).to eq(28.4)
+      end
+
     end
 
   end
