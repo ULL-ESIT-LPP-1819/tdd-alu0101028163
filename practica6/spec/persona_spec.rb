1aa6419b54fc99a1083fe6b5429dff8e8fdfa9b1
= <alu0101028163@ull.edu.es>
2018-11-22 12:42:46 +0000

    Error, no existe nombre para la persona

--- practica6/spec/persona_spec.rb
+++ practica6/spec/persona_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
-Rspec describe Persona do
+RSpec.describe Persona do
+
+  before :all do
+    @persona = Persona.new
+  end
+
+  context "Habiendo una persona" do
+    it "Debe tener un nombre" do
+      expect(@persona.nombre).to eq("Adrian")
+    end
+  end
 
 end
