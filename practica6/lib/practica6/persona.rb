c187ea63b65aabf8266fb0e89cbdfb5510f72fff
= <alu0101028163@ull.edu.es>
2018-11-22 12:48:07 +0000

    Añadida altura

--- practica6/lib/practica6/persona.rb
+++ practica6/lib/practica6/persona.rb
@@ -1,9 +1,10 @@
 class Persona
 
-  attr_reader :nombre :peso
-  def initialize(nombre, peso)
+  attr_reader :nombre, :peso, :talla
+  def initialize(nombre, peso, talla)
     @nombre = nombre
     @peso = peso
+    @talla = talla
   end
 
 end
