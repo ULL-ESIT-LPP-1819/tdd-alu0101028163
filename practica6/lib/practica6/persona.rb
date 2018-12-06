92d1fafc3b4df23ccf13958bd5051030248a40c2
= <alu0101028163@ull.edu.es>
2018-12-06 18:45:13 +0000

    Comentada la clase Persona

--- practica6/lib/practica6/persona.rb
+++ practica6/lib/practica6/persona.rb
@@ -1,6 +1,12 @@
 class Persona
 
   attr_reader :nombre, :peso, :talla, :edad, :sexo
+  # @param nombre [number] nombre del paciente.
+  # @param peso [number] peso del paciente.
+  # @param talla [number] altura del paciente en metros.
+  # @param edad [number] edad del paciente.
+  # @param sexo [number] sexo del paciente, 0 corresponde a hombre y 1 a mujer.
+  # @return [Paciente] retorna una instancia de la clase Persona.
   def initialize(nombre, peso, talla, edad, sexo)
     @nombre = nombre
     @peso = peso
@@ -9,6 +15,7 @@ class Persona
     @sexo = sexo
   end
 
+  # @return [String] retorna una cadena con la información de la Persona.
   def to_s
     s = "Nombre: #{@nombre}
      Peso: #{@peso}
