83c8c9004a327d5373f8cd37aae27843f572fe92
= <alu0101028163@ull.edu.es>
2018-12-11 21:26:10 +0000

    No existe el método para calcular el gasto energético total

--- practica6/spec/menu_spec.rb
+++ practica6/spec/menu_spec.rb
@@ -9,10 +9,16 @@ RSpec.describe "generador_menu" do
     expect(peso_teorico(@registro.talla)).to eq(63.5)
     end
     it "debe poder calcular el gasto energético basal" do
-    expect(gasto_basal(@registro.peso, @registro.talla, @registro.edad, @registro.sexo)).to eq(1755)
+    expect(GASTO_BASAL.call(@registro.peso, @registro.talla, @registro.edad, @registro.sexo)).to eq(1755)
     end
     it "debe poder calcular el efecto termógeno" do
-    expect(efecto_termogeno(@registro.peso, @registro.talla, @registro.edad, @registro.sexo)).to eq(175.5)
+    expect(efecto_termogeno(@registro.peso, @registro.talla, @registro.edad, @registro.sexo, GASTO_BASAL)).to eq(175.5)
+    end
+    it "debe poder calcular el gasto de actividad física" do
+    expect(gasto_actividad_fisica(@registro.peso, @registro.talla, @registro.edad, @registro.sexo, GASTO_BASAL, 0.12)).to eq(210.6)
+    end
+    it "debe poder calcular el gasto energético total" do
+    expect(gasto_total(@registro.peso, @registro.talla, @registro.edad, @registro.sexo, GASTO_BASAL, 0.12)).to eq(2141.1)  
     end
   end
 end
