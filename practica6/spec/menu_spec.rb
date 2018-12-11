3642f649cf54669f5d9e83b39ee3e8c58899567a
= <alu0101028163@ull.edu.es>
2018-12-11 18:45:30 +0000

    No se ha implementado el método para clacular el gasto energético basal

--- practica6/spec/menu_spec.rb
+++ practica6/spec/menu_spec.rb
@@ -1,7 +1,15 @@
 require "spec_helper"
 
 RSpec.describe "generador_menu" do
-  it "El generador de menú debe poder calcular el peso teórico" do
-  expect(peso_teorico(168)).to eq(63.5)
- end
+  before :all do
+    @registro = Registro.new("Adrian", 80, 168, 20, 0, 132.3 , 122.3, 140.3 , 12.3 , 15.2, 11.4, 10.5)
+  end
+  context "El generador de menú" do
+    it "debe poder calcular el peso teórico ideal" do
+    expect(peso_teorico(@registro.talla)).to eq(63.5)
+    end
+    it "debe poder calcular el gasto energético basal" do
+      
+    end
+  end
 end
